26c394110779631586d214337ee46af9c95cae6b
Tung Nguyen <tung.nguyen@scopicsoftware.com>
2018-09-20 10:52:47 +0700

    binary tree simple

---
+++ ComputerSience/BinaryTree.FirstChapter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree.FirstChapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Node<T>
+    {
+        public Node<T> LeftNode { get; set; }
+
+        public Node<T> RightNode { get; set; }
+
+        private T Value { get; set; }
+
+        public T GetValue()
+        {
+            return Value;
+        }
+
+        public Node(T value, Node<T> leftNode, Node<T> rightNode)
+        {
+            Value = value;
+            LeftNode = leftNode;
+            RightNode = rightNode;
+        }
+    }
+}
