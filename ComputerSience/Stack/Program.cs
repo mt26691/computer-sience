fd2e611ad5ea9435c05e7ae68e47ff0aa4849346
Tung Nguyen <tung.nguyen@scopicsoftware.com>
2018-09-20 09:00:27 +0700

    stack implementation

--- ComputerSience/Stack/Program.cs
+++ ComputerSience/Stack/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Stack
 {
@@ -6,7 +7,34 @@ namespace Stack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stack = new BimStack<int>();
+            stack.Push(7);
+            stack.Push(8);
+            Console.WriteLine(stack.Pop());
+            Console.ReadLine();
+        }
+    }
+
+    public class BimStack<T>
+    {
+        private List<T> stack = new List<T>();
+
+        public void Push(T t)
+        {
+            stack.Add(t);
+        }
+
+        public T Pop()
+        {
+            var lastItem = stack[stack.Count - 1];
+            stack.Remove(lastItem);
+            return lastItem;
+        }
+
+        public T Peek()
+        {
+            var lastItem = stack[stack.Count - 1];
+            return lastItem;
         }
     }
 }
