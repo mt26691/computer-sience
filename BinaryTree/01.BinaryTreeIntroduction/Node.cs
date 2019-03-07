e9e9820cc7ef2be83bdbaec9c116044292155e82
Nguyen Manh Tung <tung.nguyen@scopicsoftware.com>
2019-03-07 11:04:03 +0700

    init binary tree

---
+++ BinaryTree/01.BinaryTreeIntroduction/Node.cs
@@ -0,0 +1,26 @@
+﻿namespace _01.BinaryTreeIntroduction
+{
+    /// <summary>
+    /// A node contains a value and pointer to left node and right node
+    /// </summary>
+    public class Node
+    {
+        private int value;
+        private Node leftNode;
+        private Node rightNode;
+
+        public Node(int value, Node leftNode, Node rightNode)
+        {
+            // information that being stored in the tree
+            // in this example, we assume that binary tree store the int value
+            this.value = value;
+            this.leftNode = leftNode; // left child (subtree)
+            this.rightNode = rightNode; // right child (subtree)
+        }
+
+        public int GetVaue()
+        {
+            return value;
+        }
+    }
+}
---
+++ BinaryTree/01.BinaryTreeIntroduction/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01.BinaryTreeIntroduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
